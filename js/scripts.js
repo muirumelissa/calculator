0d954b6f07e4c28eb0bd59822305f17cf16d2982
melissa.muiru@student.moringaschool.com <melissa.muiru@student.moringaschool.com>
2021-07-15 15:34:38 +0300

    add calculator.html

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var add=function (number1,number2){
+    return number1+ number2; 
+    };
+    var subtract=function(number1,number2){
+    return number1 - number2
+    };
+    alert(subtract(10,5));
+    var add =function(number1,number2){
+    return number1+number2;
+    };
+    var number1=prompt("11");
+    var number2=prompt("7");
+    alert(add(number1,number2));
+    var add=function(number1,number2){
+    return number1+number2;
+    };
+    var number1=parseInt("11");
+    var number2=parseInt("7");
